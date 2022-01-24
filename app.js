bd7d9c0fad109acc2611561a8861154e1125d501
Giorgi Keshikashvili <keshikashviligio@gmail.com>
2022-01-24 23:54:45 +0400

    Add instructions

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ const last_name = document.querySelector('#last_name');
 const zip = document.querySelector('#zip');
 const gender = document.querySelector('#gender');
 const status = document.querySelector('#status');
+const user_id = document.querySelector('#user_id');
 // const position = document.querySelector('#position');
 
 const usernameError = document.querySelector('#username-error');
@@ -304,15 +305,15 @@ resolvePromise();
 // console.log();
 // console.log('Bla bla');
 
-function getUsers() {
-  const getUsersRequest = fetch('http://api.kesho.me/v1/user-test');
-
-  getUsersRequest.then(response => {
-    return response.json();
-  }).then(data => {
-    console.log(data);
-  });
-}
+// function getUsers() {
+//   const getUsersRequest = fetch('http://api.kesho.me/v1/user-test');
+//
+//   getUsersRequest.then(response => {
+//     return response.json();
+//   }).then(data => {
+//     console.log(data);
+//   });
+// }
 // getUsers();
 
 // function createUser(userData) {
@@ -336,36 +337,37 @@ function getUsers() {
 // const myFunc = async () => {
 //
 // }
-async function createUser(userData) {
-  const createUserRequest = fetch('http://api.yii2-starter-kit.localhost/v1/user-test/create', {
-    method: 'POST',
-    body: JSON.stringify(userData),
-    headers: {
-      'Content-type': 'application/json'
-    }
-  });
-  try {
-    const response = await createUserRequest;
-    const data = await response.json();
-    console.log(data);
-  } catch (error){
-   console.log(error);
-  }
-
-
-  // createUserRequest.then(response => {
-  //   return response.json();
-  // }).then(data => {
-  //   console.log(data);
-  // }).catch(error => {
-  //   console.log('Save error', error);
-  // });
-}
+// async function createUser(userData) {
+//   const createUserRequest = fetch('http://api.yii2-starter-kit.localhost/v1/user-test/create', {
+//     method: 'POST',
+//     body: JSON.stringify(userData),
+//     headers: {
+//       'Content-type': 'application/json'
+//     }
+//   });
+//   try {
+//     const response = await createUserRequest;
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error){
+//    console.log(error);
+//   }
+//
+//
+//   // createUserRequest.then(response => {
+//   //   return response.json();
+//   // }).then(data => {
+//   //   console.log(data);
+//   // }).catch(error => {
+//   //   console.log('Save error', error);
+//   // });
+// }
 
 
 signupForm.addEventListener('submit', e => {
   e.preventDefault();
   const userData = {
+    id: user_id.value,
     first_name: first_name.value,
     last_name: last_name.value,
     zip: zip.value,
@@ -375,6 +377,76 @@ signupForm.addEventListener('submit', e => {
     email: email.value,
     status: status.value,
   };
-  createUser(userData);
+  // createUser(userData); TODO: თუ user_id.value არის ცარიელი მაშინ უნდა შევქმნათ
+  // updateUser(userData); TODO: თუ user_id.value არის მაშინ უნდა დავაედიტოთ
   console.log('Save user');
 });
+
+// TODO: დაასრულეთ შემდეგი ფუნქციები
+// დაგჭირდებათ მოდალი სადაც ფორმი უნდა ჩასვათ
+function renderUsers(usersArray) {
+  // TODO: usersArray არის სერვერიდან დაბრუნებული ობიექტების მასივი
+  // TODO: ამ მონაცმების მიხედვით html ში ჩასვით ცხრილი როგორც "ცხრილი.png" შია
+  console.log(usersArray);
+  userActions(); // ყოველ რენდერზე ახლიდან უნდა მივაბათ ივენთ ლისნერები
+}
+
+// TODO: დაასრულე
+function userActions(){
+  // 1. ცხრილში ღილაკებზე უნდა მიამაგროთ event listener-ები
+  // 2. იქნება 2 ღილაკი რედაქტირება და წაშლა როგორც "ცხრილი.png" ში ჩანს
+  // 3. id შეინახეთ data-user-id ატრიბუტად ღილაკებზე
+  // 4. წაშლა ღილაკზე დაჭერისას უნდა გაიგზავნოს წაშლის მოთხოვნა (deleteUser ფუნქციის მეშვეობით) სერვერზე და გადაეცეს id
+  // 5. ეიდტის ღილაკზე უნდა გაიხსნას მოდალი სადაც ფორმი იქნება იმ მონაცემებით შევსებული რომელზეც მოხდა კლიკი
+  // ეიდტის ღილაკზე უნდა გამოიძახოთ getUser ფუნქცია და რომ დააბრუნებს ერთი მომხმარებლის დატას (ობიექტს და არა მასივს)
+  // ეს დატა უნდა შეივსოს ფორმში
+}
+
+async function getUsers(){
+  try {
+    const response = await fetch('http://api.kesho.me/v1/user-test/index');
+    const users = await response.json();
+    renderUsers(users);
+  } catch (e){
+    console.log('Error - ', e);
+  }
+}
+getUsers();
+/**
+ *
+ * შეგიძლიათ await response.json() დააბრუნოთ და საიდანაც გამოიძახებთ ამ ფუნქციას,
+ * ისიც async უნდა იყოს და იქაც await უნდა დაუწეროთ რომ დატა დაგიბრუნოთ
+ *
+ * @param userId
+ * @returns {Promise<void>}
+ */
+async function getUser(userId) {
+  try {
+    // TODO: დაასრულე
+    // `http://api.kesho.me/v1/user-test/view?id=${userId}`
+    // const data = await response.json();
+  } catch (e) {
+    console.log('Error - ', e);
+  }
+}
+async function createUser(userData){
+  try {
+    const response = await fetch('http://api.kesho.me/v1/user-test/create', {
+      method: 'post',
+      body: JSON.stringify(userData),
+      headers: {'Content-Type': 'application/json'}
+    });
+    await response.json();
+    getUsers(); // TODO: შენახვის ედიტირების და წაშლის შემდეგ ახლიდან წამოიღეთ დატა
+  }catch (e){
+    console.log('Error - ', e);
+  }
+}
+async function updateUser(userObject) {
+  // TODO: დაასრულე
+  // method POST http://api.kesho.me/v1/user-test/update?id=userObject.id
+}
+async function deleteUser(userId) {
+  // TODO დაასრულე
+  // method DELETE `http://api.kesho.me/v1/user-test/delete?id=${userId}`
+}
